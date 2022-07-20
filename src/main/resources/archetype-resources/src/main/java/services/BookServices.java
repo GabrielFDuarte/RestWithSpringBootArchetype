98deeaed02a4c33f8ecb3bdbee9c50f9d3eb91dc
GabrielFDuarte <enggfduarte@gmail.com>
2022-07-20 11:03:52 -0300

    Working with Archetypes

---
+++ src/main/resources/archetype-resources/src/main/java/services/BookServices.java
@@ -0,0 +1,59 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ${package}.converter.DozerConverter;
+import ${package}.data.model.Book;
+import ${package}.data.vo.v1.BookVO;
+import ${package}.exception.ResourceNotFoundException;
+import ${package}.repository.BookRepository;
+
+@Service
+public class BookServices {
+	
+	@Autowired
+	BookRepository repository;
+		
+	public BookVO create(BookVO book) {
+		var entity = DozerConverter.parseObject(book, Book.class);
+		var vo = DozerConverter.parseObject(repository.save(entity), BookVO.class);
+		return vo;
+	}
+	
+	public List<BookVO> findAll() {
+		return DozerConverter.parseListObjects(repository.findAll(), BookVO.class);
+	}	
+	
+	public BookVO findById(Long id) {
+
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		return DozerConverter.parseObject(entity, BookVO.class);
+	}
+		
+	public BookVO update(BookVO book) {
+		var entity = repository.findById(book.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		entity.setAuthor(book.getAuthor());
+		entity.setLaunchDate(book.getLaunchDate());
+		entity.setPrice(book.getPrice());
+		entity.setTitle(book.getTitle());
+		
+		var vo = DozerConverter.parseObject(repository.save(entity), BookVO.class);
+		return vo;
+	}	
+	
+	public void delete(Long id) {
+		Book entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		repository.delete(entity);
+	}
+
+}
